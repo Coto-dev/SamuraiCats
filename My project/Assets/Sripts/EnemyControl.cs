693084f0221fca7eca91388fb0c1cf5be49b9944
coto1488 <shorop5@mail.ru>
2022-05-31 13:41:53 +0700

    бегает атакует, пока просто анимация

--- My project/Assets/Sripts/EnemyControl.cs
+++ My project/Assets/Sripts/EnemyControl.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,26 +9,45 @@ public class EnemyControl : MonoBehaviour
     public Transform player;
     public float speed;
     public float agroDistance;
-
+    Animator animator;
+    int attackRange = 1;
     // Start is called before the first frame update
     void Start()
     {
         physic = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float distToPlayer = Vector2.Distance(transform.position, player.position);
-        if (distToPlayer < agroDistance)
+       
+         if(Math.Abs(transform.position.x - player.position.x) < attackRange && Math.Abs(transform.position.y - player.position.y) < attackRange)
+        {
+            animator.SetBool("Attack", true);
+            animator.SetBool("Walk", false);
+        }
+       else if (distToPlayer < agroDistance)
         {
             StartHunting();
         }
-      
+        else if (distToPlayer > agroDistance)
+        {
+            StopHunting();
+        }
+
+    }
+    void StopHunting()
+    {
+        animator.SetBool("Walk", false);
+        animator.SetBool("Idle", true);
+        animator.SetBool("Attack", false);
     }
     void StartHunting()
     {
-
+        animator.SetBool("Attack", false);
+        animator.SetBool("Walk", true);
         //physic.MovePosition(physic.position + direction * speed * Time.fixedDeltaTime);
         if (player.position.x > transform.position.x)
         {
@@ -39,26 +59,7 @@ public class EnemyControl : MonoBehaviour
 
         }
         transform.position = Vector2.MoveTowards(transform.position, player.position, speed*Time.deltaTime);
-        /*  if (player.position.x < transform.position.x)
-          {
-              physic.velocity = new Vector2(-speed, 0);
-          }
-          else if (player.position.y > transform.position.y)
-          {
-              Debug.Log("down");
-              physic.velocity = new Vector2(0, speed);
-          }
-          else if (player.position.x > transform.position.x)
-          {
-              physic.velocity = new Vector2(speed, 0);
-
-          }
-          else if (player.position.y < transform.position.y)
-          {
-              physic.velocity = new Vector2(0, speed);
-              // Debug.Log("down");
-          }*/
-
+      
     }
 
 }
